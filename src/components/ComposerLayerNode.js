91500ce4a0b3bc317732d11f60c37d02249ac5d0
Colm Tuite <colmtuite@gmail.com>
2018-08-06 19:02:18 +0100

    Finished toolbelt. Progress on properties.

--- src/components/ComposerLayerNode.js
+++ src/components/ComposerLayerNode.js
@@ -1,11 +1,6 @@
-import React from 'react';
 import styled, { css } from 'styled-components';
 import { Theme } from 'radix-ds';
 import {
-  Flexbox,
-  Box,
-  Text,
-  List,
   ListItem
 } from 'radix-ds';
 
@@ -22,57 +17,9 @@ const Layer = styled(ListItem)`
     background-color: ${Theme.GRAY_200};
   }
 
-  ${props => props.component && css`
-    background-color: ${Theme.RED_500};
+  ${props => props.level2 && css`
+    padding-left: 35px;
   `}
 `;
 
-function ComposerLayerNode() {
-  return (
-    <Layer>
-      <Box
-        mr1
-      >
-        <svg
-          width="15"
-          height="15"
-          viewBox="0 0 15 15"
-          fill="none"
-          stroke="currentColor"
-          xmlns="http://www.w3.org/2000/svg"
-          style={{
-            display: 'block'
-          }}
-        >
-          <path d="M0 0L4 4L0 8" transform="translate(5.5 3.5)" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-      </Box>
-      <Box
-        mr2
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="15"
-          height="15"
-          viewBox="0 0 15 15"
-          fill="none"
-          stroke="currentColor"
-          style={{
-            display: 'block'
-          }}
-        >
-          <rect x="0.5" y="0.5" width="10" height="10" rx="1.5" transform="translate(2 2)"/>
-        </svg>
-      </Box>
-      <Box>
-        <Text
-          size1
-        >
-          Button
-        </Text>
-      </Box>
-    </Layer>
-  );
-}
-
-export default ComposerLayerNode;
+export default Layer;
--- src/components/ComposerLayerTree.js
+++ src/components/ComposerLayerTree.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {
-  Flexbox,
   Box,
   List,
   Text
 } from 'radix-ds';
 
-import ComposerLayerNode from './ComposerLayerNode'
+import Layer from './ComposerLayerNode'
 
 function ComposerLayerTree() {
   return (
@@ -21,33 +20,121 @@ function ComposerLayerTree() {
       <List
         size1
       >
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
-        <ComposerLayerNode />
+        <Layer>
+          <Box
+            mr6
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              stroke="currentColor"
+              style={{
+                display: 'block'
+              }}
+            >
+              <rect x="0.5" y="0.5" width="10" height="10" rx="1.5" transform="translate(2 2)"/>
+            </svg>
+          </Box>
+          <Box>
+            <Text
+              size1
+              gray600
+            >
+              Search
+            </Text>
+          </Box>
+        </Layer>
+        <Layer>
+          <Box
+            mr1
+          >
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              stroke="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+              style={{
+                display: 'block'
+              }}
+            >
+              <path d="M0 0L4 4L0 8" transform="translate(5.5 3.5)" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </Box>
+          <Box
+            mr2
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              stroke="currentColor"
+              style={{
+                display: 'block'
+              }}
+            >
+              <rect x="0.5" y="0.5" width="10" height="10" rx="1.5" transform="translate(2 2)"/>
+            </svg>
+          </Box>
+          <Box>
+            <Text
+              size1
+            >
+              Button
+            </Text>
+          </Box>
+        </Layer>
+        <Layer
+          level2
+        >
+          <Box
+            mr1
+          >
+            <svg
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              stroke="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+              style={{
+                display: 'block'
+              }}
+            >
+              <path d="M0 0L4 4L0 8" transform="translate(5.5 3.5)" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </Box>
+          <Box
+            mr2
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+              stroke="currentColor"
+              style={{
+                display: 'block'
+              }}
+            >
+              <rect x="0.5" y="0.5" width="10" height="10" rx="1.5" transform="translate(2 2)"/>
+            </svg>
+          </Box>
+          <Box>
+            <Text
+              size1
+            >
+              Button
+            </Text>
+          </Box>
+        </Layer>
       </List>
     </Box>
   );

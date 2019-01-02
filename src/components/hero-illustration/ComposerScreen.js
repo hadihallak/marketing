5d0d0bb0d5f96b8ec008ab7e01d2100742146ff8
Colm Tuite <colmtuite@gmail.com>
2019-01-02 08:39:03 +0000

    Putting the house back together brick by brick

--- src/components/hero-illustration/ComposerScreen.js
+++ src/components/hero-illustration/ComposerScreen.js
@@ -59,7 +59,185 @@ const ComposerScreen = () => (
   >
     <AspectRatio ratio_169>
       <Browser>
-        
+        <Flexbox
+          height_100
+        >
+          <Box fs_0 br bcgray400 style={{width: '225px'}}>
+            <Flexbox
+              height_100
+              fd_column
+            >
+              <Box padding_1 bb bcgray400>
+                <Flexbox
+                  jc_spacebetween
+                >
+                  <GhostButton size2 tabIndex="-1">
+                    <svg
+                      width="25"
+                      height="25"
+                      viewBox="0 0 25 25"
+                      fill="none"
+                      stroke="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M4 18.5H21"/>
+                      <path d="M4 12.5H21"/>
+                      <path d="M4 6.5H21"/>
+                    </svg>
+                  </GhostButton>
+                </Flexbox>
+              </Box>
+              <Box overflow_hidden>
+                <Box>
+                  <List mt_1 mb_1>
+                    <li>
+                      <PropNode label="Button" active />
+                    </li>
+                  </List>
+                </Box>
+                <Box
+                  bt
+                  bcgray300
+                >
+                  <PropHeader label="Size" />
+                  <List mt_1 mb_1>
+                    <li>
+                      <PropNode label="Size 1" />
+                    </li>
+                    <li>
+                      <PropNode label="Size 2" />
+                    </li>
+                  </List>
+                </Box>
+                <Box
+                  bt
+                  bcgray300
+                >
+                  <PropHeader label="Color" />
+                  <List mt_1 mb_1>
+                    <li>
+                      <PropNode label="Gray" />
+                    </li>
+                    <li>
+                      <PropNode label="Blue" />
+                    </li>
+                    <li>
+                      <PropNode label="Green" />
+                    </li>
+                    <li>
+                      <PropNode label="Red" />
+                    </li>
+                  </List>
+                </Box>
+                <Box
+                  bt
+                  bcgray300
+                >
+                  <PropHeader label="Alignment" />
+                  <List mt_1 mb_1>
+                    <li>
+                      <PropNode label="Left" />
+                    </li>
+                    <li>
+                      <PropNode label="Middle" />
+                    </li>
+                    <li>
+                      <PropNode label="Right" />
+                    </li>
+                  </List>
+                </Box>
+                <Box
+                  bt
+                  bcgray300
+                >
+                  <List mt_1 mb_1>
+                    <li>
+                      <PropNode label="Active" />
+                    </li>
+                  </List>
+                </Box>
+                <Box
+                  bt
+                  bcgray300
+                >
+                  <List mt_1 mb_1>
+                    <li>
+                      <PropNode label="Waiting" />
+                    </li>
+                  </List>
+                </Box>
+                <Box
+                  bt
+                  bcgray300
+                >
+                  <List mt_1 mb_1>
+                    <li>
+                      <PropNode label="Disabled" />
+                    </li>
+                  </List>
+                </Box>
+              </Box>
+            </Flexbox>
+          </Box>
+
+
+
+
+          
+          
+          
+          
+          <Box fs_0 bl bcgray400 style={{width: '225px'}}>
+            <Flexbox
+              height_100
+              fd_column
+            >
+              <Box padding_1
+                pr_2
+                bb
+                bcgray400
+              >
+                <Flexbox ai_center jc_flexend>
+                  <GhostButton size2 tabIndex="-1">
+                    <svg
+                      width="25"
+                      height="25"
+                      viewBox="0 0 25 25"
+                      fill="none"
+                      stroke="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                    <circle cx="12.5" cy="12.5" r="3"/>
+                    <circle cx="12.5" cy="12.5" r="8"/>
+                    <path d="M6.5 18.5L10.5 14.5"/>
+                    <path d="M14.5 10.5L18.5 6.5"/>
+                    <path d="M10.5 10.5L6.5 6.5"/>
+                    <path d="M18 18L14.5 14.5"/>
+                    </svg>
+                  </GhostButton>
+                  <GhostButton size2 tabIndex="-1">
+                    <svg
+                      width="25"
+                      height="25"
+                      viewBox="0 0 25 25"
+                      fill="none"
+                      stroke="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M9.55001 18.5H15.45C15.2184 19.6411 14.2095 20.5 13 20.5H12C10.7905 20.5 9.78164 19.6411 9.55001 18.5Z"/>
+                      <path d="M5.5 15V18.5H19.5V15L17.1667 13.5V10C17.1667 10 17.5 4.5 12.5 4.5C7.5 4.5 7.83333 10 7.83333 10V13.5L5.5 15Z" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
+                  </GhostButton>
+                  <Box pl_2>
+                    <Avatar size2 url="https://pbs.twimg.com/profile_images/864164353771229187/Catw6Nmh_bigger.jpg"/>
+                  </Box>
+                </Flexbox>
+              </Box>
+              <States />
+              <ComposerProperties />
+            </Flexbox>
+          </Box>
+        </Flexbox>
       </Browser>
     </AspectRatio>
   </Figure>
